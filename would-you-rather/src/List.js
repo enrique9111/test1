d25395ba0e34819870af1d36b624a2b7d0a9f450
enrique <enriquecabrera32123@gmail.com>
2024-05-17 15:23:51 -0700

    test

---
+++ would-you-rather/src/List.js
@@ -0,0 +1,26 @@
+
+
+
+
+
+
+const list = [
+  {left: {description: "Speak all languages fluently", wins: 0}, right: {description: "Be able to play all musical instruments perfectly", wins: 0}},
+  {left: {description: "Have the power of invisibility", wins: 0}, right: {description: "Have the power of flight", wins: 0}},
+  {left: {description: "Be immortal", wins: 0}, right: {description: "Have unlimited wealth", wins: 0}},
+  {left: {description: "Be able to control time", wins: 0}, right: {description: "Be able to control the elements (earth, air, fire, water)", wins: 0}},
+  {left: {description: "Have the ability to teleport anywhere instantly", wins: 0}, right: {description: "Have the ability to read minds", wins: 0}},
+  {left: {description: "Never need to sleep", wins: 0}, right: {description: "Never need to eat", wins: 0}},
+  {left: {description: "Have the power to heal any injury or illness", wins: 0}, right: {description: "Have the power to bring the dead back to life", wins: 0}},
+  {left: {description: "Be the best athlete in the world", wins: 0}, right: {description: "Be the smartest person in the world", wins: 0}},
+  {left: {description: "Live in a world with no technology", wins: 0}, right: {description: "Live in a world with no nature", wins: 0}},
+  {left: {description: "Experience the beginning of the universe", wins: 0}, right: {description: "Experience the end of the universe", wins: 0}},
+  {left: {description: "Always know the truth but never be believed", wins: 0}, right: {description: "Always be believed but never know the truth", wins: 0}},
+  {left: {description: "Live in a world with constant daylight", wins: 0}, right: {description: "Live in a world with constant darkness", wins: 0}},
+]
+
+
+
+
+
+export default list
